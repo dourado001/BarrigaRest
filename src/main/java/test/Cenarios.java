078a97d6614581a38437a519b428a809fbb41a87
Guilherme Barbosa <guilherme.dourado95@gmail.com>
2021-11-14 22:55:36 -0300

    Refatoração do body das requests, ordem de execução e gerenciamento de massa 1

--- src/main/java/test/Cenarios.java
+++ src/main/java/test/Cenarios.java
@@ -2,18 +2,24 @@ package test;
 
 import core.BaseTest;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import utils.DataUtils;
 
-import java.util.Calendar;
 import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Cenarios extends BaseTest {
 
     private int idConta;
     private String TOKEN;
+    private static String CONTA_NAME = "Conta" + System.nanoTime();
+    private static Integer CONTA_ID;
+    private static Integer MOVIMENTACAO_ID;
 
     @Before
     public void login() {
@@ -31,7 +37,7 @@ public class Cenarios extends BaseTest {
     }
 
     @Test
-    public void naoDeveAcessarSemTOKEN() {
+    public void CT01_naoDeveAcessarSemTOKEN() {
         given()
                 .when()
                 .get("/contas")
@@ -40,60 +46,38 @@ public class Cenarios extends BaseTest {
         ;
     }
 
-    //Cenario 2 - Deve incluir uma conta com sucesso
-    //post/signin email, senha
-    //post/contas
     @Test
-    public void deveIncluirConta() {
-        idConta = given()
+    public void CT02_deveIncluirConta() {
+        CONTA_ID = given()
                 .header("Authorization", "JWT " + TOKEN)
-                .body("{\"nome\" : \"Conta base 2\"}")
+                .body("{\"nome\" : \""+ CONTA_NAME +"\"}")
                 .when()
                 .post("/contas")
                 .then()
                 .log().all()
                 .statusCode(201)
-                .body("nome", is("Conta base 2"))
                 .extract().path("id")
         ;
     }
 
-    //Cenario 3 - Deve alterar uma conta com sucesso
-    //PUT/contas/:id
     @Test
-    public void deveAlterarConta() {
-        idConta = given()
+    public void CT03_deveAlterarConta() {
+                given()
                 .header("Authorization", "JWT " + TOKEN)
-                .body("{\"nome\" : \"Conta base 2\"}")
+                .body("{\"nome\" : \""+CONTA_NAME+" alterada"+"\"}")
                 .when()
-                .post("/contas")
-                .then()
-                .log().all()
-                .statusCode(201)
-                .body("nome", is("Conta base 2"))
-                .extract().path("id")
-        ;
-
-        given()
-                .header("Authorization", "JWT " + TOKEN)
-                .body("{\"nome\" : \"Conta automação\"}")
-                .when()
-                .put("/contas/" + idConta)
+                .put("/contas/"+CONTA_ID)
                 .then()
                 .log().all()
                 .statusCode(200)
-                .body("nome", is("Conta automação"))
         ;
     }
 
-
-    //Cenario 4 - Não deve incluir conta com nome repetido
-    //POST/contas/
     @Test
-    public void naoDeveIncluirContaNomeRepetido() {
+    public void CT04_naoDeveIncluirContaNomeRepetido() {
         given()
                 .header("Authorization", "JWT " + TOKEN)
-                .body("{\"nome\" : \"Conta base 2\"}")
+                .body("{\"nome\" : \""+CONTA_NAME+" alterada"+"\"}")
                 .when()
                 .post("/contas")
                 .then()
@@ -103,56 +87,23 @@ public class Cenarios extends BaseTest {
         ;
     }
 
-    //Cenario 5 - Deve inserir movimentação com sucesso
-    //POST/transações
-    //conta_id, descricao(descrição basica da transacao), envolvido(Nome da pessoa),
-    // tipo(DESP / REC), data_transacao(dd/MM/YYYY), data_pagamento(dd/MM/YYYY),
-    // valor(0.00f), status(true/false)
     @Test
-    public void deveInserirMovimentacaoComSucesso() {
-        Movimentacao mov = new Movimentacao();
+    public void CT05_deveInserirMovimentacaoComSucesso() {
+        Movimentacao mov = getMovimentacaoValida();
 
-        mov.setDescricao("Movimentação de dinheiro de bitcoin");
-        mov.setEnvolvido("Mané Pelé");
-        mov.setTipo("REC");
-        mov.setData_transacao("05/11/2021");
-        mov.setData_pagamento("04/11/2021");
-        mov.setValor(2500f);
-        mov.setStatus(true);
-
-        String nomeConta = "Vermeinha";
-
-        idConta = given()
-                .header("Authorization", "JWT " + TOKEN)
-                .body("{\"nome\" : \"" + nomeConta + "\"}")
-                .when()
-                .post("/contas")
-                .then()
-                .log().all()
-                .statusCode(201)
-                .body("nome", is(nomeConta))
-                .extract().path("id")
-        ;
-
-        mov.setConta_id(idConta);
-
-        given()
+        MOVIMENTACAO_ID = given()
                 .header("Authorization", "JWT " + TOKEN)
                 .body(mov)
                 .when()
                 .post("/transacoes")
                 .then()
                 .statusCode(201)
-                .body("envolvido", is("Mané Pelé"))
-                .body("valor", equalTo("2500.00"))
-                .body("descricao", is("Movimentação de dinheiro de bitcoin"))
+                .extract().path("id")
         ;
     }
 
-    //Cenario 6 - Deve validar campos obrigatorios na movimentação
-    //POST/transações
     @Test
-    public void deveValidarCamposObrigatoriosMovimentacao() {
+    public void CT06_deveValidarCamposObrigatoriosMovimentacao() {
         given()
                 .header("Authorization", "JWT " + TOKEN)
                 .when()
@@ -173,28 +124,10 @@ public class Cenarios extends BaseTest {
         ;
     }
 
-    //Cenario 7 - Não deve cadastrar movimentação futura
-    //POST/transações data futura a data atual
     @Test
-    public void naoDeveCadastrarMovimentacaoFutura() {
+    public void CT07_naoDeveCadastrarMovimentacaoFutura() {
         Movimentacao mov = getMovimentacaoValida();
-        mov.setData_transacao("11/11/2021");
-
-        String nomeConta = "Verdin";
-
-        idConta = given()
-                .header("Authorization", "JWT " + TOKEN)
-                .body("{\"nome\" : \"" + nomeConta + "\"}")
-                .when()
-                .post("/contas")
-                .then()
-                .log().all()
-                .statusCode(201)
-                .body("nome", is(nomeConta))
-                .extract().path("id")
-        ;
-
-        mov.setConta_id(idConta);
+        mov.setData_transacao(DataUtils.getDataDiferencaDias(2));
 
         given()
                 .header("Authorization", "JWT " + TOKEN)
@@ -208,195 +141,77 @@ public class Cenarios extends BaseTest {
         ;
     }
 
-    //Cenario 8 - Não deve remover conta com movimentação
-    //DELETE/contas/:id
     @Test
-    public void naoDeveRemoverContaMovimentacao() {
-        String nomeConta = "Roxin";
-
-        idConta = given()
-                .header("Authorization", "JWT " + TOKEN)
-                .body("{\"nome\" : \"" + nomeConta + "\"}")
-                .when()
-                .post("/contas")
-                .then()
-                .log().all()
-                .statusCode(201)
-                .body("nome", is(nomeConta))
-                .extract().path("id")
-        ;
-
-        given()
-                .header("Authorization", "JWT " + TOKEN)
-                .body("{\n" +
-                        "    \"conta_id\" : \"" + idConta + "\",\n" +
-                        "    \"descricao\" : \"Movimentação de dinheiro de bitcoin\",\n" +
-                        "    \"envolvido\" : \"Mané Pelé\",\n" +
-                        "    \"tipo\" : \"REC\",\n" +
-                        "    \"data_transacao\" : \"05/11/2021\",\n" +
-                        "    \"data_pagamento\" : \"04/11/2021\",\n" +
-                        "    \"valor\" : 10025.75,\n" +
-                        "    \"status\" : false\n" +
-                        "}")
-                .when()
-                .post("/transacoes")
-                .then()
-                .statusCode(201)
-                .body("envolvido", is("Mané Pelé"))
-                .body("valor", equalTo("10025.75"))
-                .body("descricao", is("Movimentação de dinheiro de bitcoin"))
-        ;
+    public void CT08_naoDeveRemoverContaMovimentacao() {
 
         given()
                 .header("Authorization", "JWT " + TOKEN)
                 .when()
-                .delete("/contas/" + idConta)
+                .pathParam("id",CONTA_ID)
+                .delete("/contas/{id}")
                 .then()
                 .statusCode(500)
-                .body("detail", hasItem("Key (id)=(" + idConta + ") is still referenced from table \"transacoes\"."))
+                .body("detail", is("Key (id)=(" + CONTA_ID + ") is still referenced from table \"transacoes\"."))
         ;
-
     }
 
-    //Cenario 9 - Deve calcular saldo contas
-    //GET/saldo
     @Test
-    public void deveCalcularSaldoContas() {
-        String nomeConta = "Marromzinho";
-
-        idConta = given()
-                .header("Authorization", "JWT " + TOKEN)
-                .body("{\"nome\" : \"" + nomeConta + "\"}")
-                .when()
-                .post("/contas")
-                .then()
-                .log().all()
-                .statusCode(201)
-                .body("nome", is(nomeConta))
-                .extract().path("id")
-        ;
+    public void CT09_deveCalcularSaldoContas() {
+        Movimentacao mov1 = getMovimentacaoValida();
+        mov1.setValor(200.00f);
+        Movimentacao mov2 = getMovimentacaoValida();
+        mov2.setValor(300.00f);
 
         given()
                 .header("Authorization", "JWT " + TOKEN)
-                .body("{\n" +
-                        "    \"conta_id\" : \"" + idConta + "\",\n" +
-                        "    \"descricao\" : \"Movimentação do dinheiro dos cursos da hotmart\",\n" +
-                        "    \"envolvido\" : \"Sané Mané Pelé\",\n" +
-                        "    \"tipo\" : \"REC\",\n" +
-                        "    \"data_transacao\" : \"05/11/2021\",\n" +
-                        "    \"data_pagamento\" : \"04/11/2021\",\n" +
-                        "    \"valor\" : 5000.00,\n" +
-                        "    \"status\" : true\n" +
-                        "}")
+                .body(mov1)
                 .when()
                 .post("/transacoes")
                 .then()
                 .statusCode(201)
-                .body("envolvido", is("Sané Mané Pelé"))
-                .body("valor", equalTo("5000.00"))
-                .body("descricao", is("Movimentação do dinheiro dos cursos da hotmart"))
         ;
 
         given()
                 .header("Authorization", "JWT " + TOKEN)
-                .body("{\n" +
-                        "    \"conta_id\" : \"" + idConta + "\",\n" +
-                        "    \"descricao\" : \"Reforma do banheiro\",\n" +
-                        "    \"envolvido\" : \"MSN\",\n" +
-                        "    \"tipo\" : \"DESP\",\n" +
-                        "    \"data_transacao\" : \"05/11/2021\",\n" +
-                        "    \"data_pagamento\" : \"05/11/2021\",\n" +
-                        "    \"valor\" : 2000.00,\n" +
-                        "    \"status\" : true\n" +
-                        "}")
+                .body(mov2)
                 .when()
                 .post("/transacoes")
                 .then()
                 .statusCode(201)
-                .body("envolvido", is("MSN"))
-                .body("valor", equalTo("-2000.00"))
-                .body("descricao", is("Reforma do banheiro"))
         ;
 
         given()
                 .header("Authorization", "JWT " + TOKEN)
                 .when()
-                .get("/saldo/" + idConta)
+                .get("/saldo")
                 .then()
                 .log().all()
                 .statusCode(200)
-                .body("conta_id", is(idConta))
-                .body("conta", is(nomeConta))
-                .body("saldo", is("3000.00"))
+                .body("find{it.conta_id == "+ CONTA_ID +"}.saldo", is("3000.00"))
         ;
     }
 
-    //Cenario 10 - Deve remover movimentação
-    //DELETE/transações/:id
     @Test
-    public void deveRemoverTransacao() {
-        String nomeConta = "Amarelin";
-
-        idConta = given()
-                .header("Authorization", "JWT " + TOKEN)
-                .body("{\"nome\" : \"" + nomeConta + "\"}")
-                .when()
-                .post("/contas")
-                .then()
-                .log().all()
-                .statusCode(201)
-                .body("nome", is(nomeConta))
-                .extract().path("id")
-        ;
-
-        int transferencia_id = given()
-                .header("Authorization", "JWT " + TOKEN)
-                .body("{\n" +
-                        "    \"conta_id\" : \"" + idConta + "\",\n" +
-                        "    \"descricao\" : \"Freela do casamento da Marcinha\",\n" +
-                        "    \"envolvido\" : \"Sané Mané Pelé\",\n" +
-                        "    \"tipo\" : \"REC\",\n" +
-                        "    \"data_transacao\" : \"05/11/2021\",\n" +
-                        "    \"data_pagamento\" : \"04/11/2021\",\n" +
-                        "    \"valor\" : 5000.00,\n" +
-                        "    \"status\" : true\n" +
-                        "}")
-                .when()
-                .post("/transacoes")
-                .then()
-                .statusCode(201)
-                .body("envolvido", is("Sané Mané Pelé"))
-                .body("valor", equalTo("5000.00"))
-                .body("descricao", is("Freela do casamento da Marcinha"))
-                .extract().path("id");
+    public void CT10_deveRemoverTransacao() {
 
         given()
                 .header("Authorization", "JWT " + TOKEN)
                 .when()
-                .delete("/transacoes/" + transferencia_id)
+                .delete("/transacoes/" + MOVIMENTACAO_ID)
                 .then()
                 .statusCode(204)
-        ;
-
-        given()
-                .header("Authorization", "JWT " + TOKEN)
-                .when()
-                .get("/transacoes/" + transferencia_id)
-                .then()
-                .log().all()
-                .statusCode(404)
-                .body("error", is("Transacao com id " + transferencia_id + " não encontrada"))
         ;
     }
 
     public Movimentacao getMovimentacaoValida(){
         Movimentacao mov = new Movimentacao();
 
+        mov.setConta_id(CONTA_ID);
         mov.setDescricao("Movimentação de dinheiro de bitcoin");
         mov.setEnvolvido("Mané Pelé");
         mov.setTipo("REC");
-        mov.setData_transacao("05/11/2021");
-        mov.setData_pagamento("04/11/2021");
+        mov.setData_transacao(DataUtils.getDataDiferencaDias(-1));
+        mov.setData_pagamento(DataUtils.getDataDiferencaDias(5));
         mov.setValor(2500f);
         mov.setStatus(true);
         return mov;
